2b882f0c9706ffa8564d4bc71fd5601bc15f800d
Ruslan <RMingalievF@icloud.com>
2022-10-29 20:19:49 +0500

    create outlets

---
+++ ColorMixer/MainController.swift
@@ -0,0 +1,40 @@
+//
+//  MainController.swift
+//  ColorMixer
+//
+//  Created by Руслан Мингалиев on 29.10.2022.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+    
+    @IBOutlet weak var mixerColorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+   
+    @IBOutlet weak var valueRedLabel: UILabel!
+    @IBOutlet weak var valueGreenLabel: UILabel!
+    @IBOutlet weak var valueBlueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    
+    
+    
+    
+}
+
