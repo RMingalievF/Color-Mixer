69c27ad808f4445e717668313e2afe5b3e0bc286
Ruslan <RMingalievF@icloud.com>
2022-11-16 21:36:43 +0300

    add startVC

---
+++ ColorMixer/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartViewController.swift
+//  ColorMixer
+//
+//  Created by Руслан Мингалиев on 16.11.2022.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
