95a77aaec38b776f86fbd7d0bcdb3c9a68a37605
Ruslan <RMingalievF@icloud.com>
2023-09-29 19:26:27 +0500

    Delete view did load

--- ColorMixer/StartViewController.swift
+++ ColorMixer/StartViewController.swift
@@ -14,11 +14,6 @@ protocol MainViewControllerDelrgate {
 class StartViewController: UIViewController {
 
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let startVC = segue.destination as? MainViewController else { return }
         startVC.colorView = view.backgroundColor
